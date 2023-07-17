da20d1b89cea6e6afb6f675062d4e8f382c0ce09
Steve Meadows <f6h1sctwvu@outlook.com>
2023-07-17 17:40:51 -0500

    fix white space

--- vars/cplLabisProduction.groovy
+++ vars/cplLabisProduction.groovy
@@ -1,9 +1,9 @@
 def database() {
-	fileOperations([fileCreateOperation(fileContent: '''# MySQL. Versions 5.5.8 and up are supported.
+  fileOperations([fileCreateOperation(fileContent: '''# MySQL. Versions 5.5.8 and up are supported.
 production:
   adapter: mysql2
   database: lmo_tpcm_production
-	username: <%= ENV["MYSQL_USER_CREDS"].split(':')[0] %>
+  username: <%= ENV["MYSQL_USER_CREDS"].split(':')[0] %>
   password: <%= ENV["MYSQL_USER_CREDS"].split(':')[1] %>
   host: ussigatxmys004p.us.int.sonichealthcare
   port: 3307
@@ -12,17 +12,17 @@ production:
 mercator_production:
   adapter: mysql2
   database: mercator
-	username: <%= ENV["MYSQL_USER_CREDS"].split(':')[0] %>
+  username: <%= ENV["MYSQL_USER_CREDS"].split(':')[0] %>
   password: <%= ENV["MYSQL_USER_CREDS"].split(':')[1] %>
-	host: ussigatxmys004p.us.int.sonichealthcare
-	port: 3307
-	reconnect: true
+  host: ussigatxmys004p.us.int.sonichealthcare
+  port: 3307
+  reconnect: true
 ''', fileName: 'config/database.yml')])
 
 }
 
 def secrets() {
-	fileOperations([fileCreateOperation(fileContent: '''# Be sure to restart your server when you modify this file.
+  fileOperations([fileCreateOperation(fileContent: '''# Be sure to restart your server when you modify this file.
 production:
   secret: 9afebc7fffcade1cca5ce986cf00cc9eeb82c7b720bbd5b2d66dbf873b354f571f35b584d87649ca50d13e495e61977f5c2ede28baccdbbc96c588454c06cce6
 
@@ -30,7 +30,7 @@ production:
 }
 
 def queue_server() {
-	fileOperations([fileCreateOperation(fileContent: '''# Be sure to restart your server when you modify this file.
+  fileOperations([fileCreateOperation(fileContent: '''# Be sure to restart your server when you modify this file.
 production:
   host: rabbitmqapp
   username: labis
@@ -41,39 +41,39 @@ production:
 }
 
 def servers() {
-	fileOperations([fileCreateOperation(fileContent: '''# Be sure to restart your server when you modify this file.
+  fileOperations([fileCreateOperation(fileContent: '''# Be sure to restart your server when you modify this file.
 production:
   slide_server:
-	  host: 10.212.16.141
-	  port: 8081
+    host: 10.212.16.141
+    port: 8081
   mongo_server:
-	  host: 10.214.5.134
-	  port: 8088
+    host: 10.214.5.134
+    port: 8088
   req_viewer:
-	  host: 10.212.16.141
-	  port: 8082
+    host: 10.212.16.141
+    port: 8082
   redis_server:
-	  host: USSIGATXRED003P.US.INT.SONICHEALTHCARE
-	  port: 6379
+    host: USSIGATXRED003P.US.INT.SONICHEALTHCARE
+    port: 6379
   labis_api:
-	  host: apitest.labis.net
+    host: apitest.labis.net
   labis_v2:
-	  host: v2test.labis.net
+    host: v2test.labis.net
   labis_server:
-	  host: v2test.labis.net
+    host: v2test.labis.net
   device_server:
-	  host: localhost
-	  port: 9999
+    host: localhost
+    port: 9999
   ssrs_server:
-	  host: https://ssrstest.labis.net
-	  folder: LABIS Reports
-	  user: reportuser
-	  password: S0nic123
+    host: https://ssrstest.labis.net
+    folder: LABIS Reports
+    user: reportuser
+    password: S0nic123
 ''', fileName: 'config/servers.yml')])
 }
 
 def memcached() {
-	fileOperations([fileCreateOperation(fileContent: '''# Be sure to restart your server when you modify this file.
+  fileOperations([fileCreateOperation(fileContent: '''# Be sure to restart your server when you modify this file.
 defaults:
   readonly: false
   urlencode: false
--- vars/cplLabisSandbox.groovy
+++ vars/cplLabisSandbox.groovy
@@ -1,9 +1,9 @@
 def database() {
-	fileOperations([fileCreateOperation(fileContent: '''# MySQL. Versions 5.5.8 and up are supported.
+  fileOperations([fileCreateOperation(fileContent: '''# MySQL. Versions 5.5.8 and up are supported.
 production:
   adapter: mysql2
   database: lmo_tpcm_production
-	username: <%= ENV["MYSQL_USER_CREDS"].split(':')[0] %>
+  username: <%= ENV["MYSQL_USER_CREDS"].split(':')[0] %>
   password: <%= ENV["MYSQL_USER_CREDS"].split(':')[1] %>
   host: ussigatxmys004t.us.int.sonichealthcare
   port: 3307
@@ -12,17 +12,17 @@ production:
 mercator_production:
   adapter: mysql2
   database: mercator
-	username: <%= ENV["MYSQL_USER_CREDS"].split(':')[0] %>
+  username: <%= ENV["MYSQL_USER_CREDS"].split(':')[0] %>
   password: <%= ENV["MYSQL_USER_CREDS"].split(':')[1] %>
-	host: ussigatxmys004t.us.int.sonichealthcare
-	port: 3307
-	reconnect: true
+  host: ussigatxmys004t.us.int.sonichealthcare
+  port: 3307
+  reconnect: true
 ''', fileName: 'config/database.yml')])
 
 }
 
 def secrets() {
-	fileOperations([fileCreateOperation(fileContent: '''# Be sure to restart your server when you modify this file.
+  fileOperations([fileCreateOperation(fileContent: '''# Be sure to restart your server when you modify this file.
 production:
   secret: 9afebc7fffcade1cca5ce986cf00cc9eeb82c7b720bbd5b2d66dbf873b354f571f35b584d87649ca50d13e495e61977f5c2ede28baccdbbc96c588454c06cce6
 
@@ -30,7 +30,7 @@ production:
 }
 
 def queue_server() {
-	fileOperations([fileCreateOperation(fileContent: '''# Be sure to restart your server when you modify this file.
+  fileOperations([fileCreateOperation(fileContent: '''# Be sure to restart your server when you modify this file.
 production:
   host: rabbitmqapp
   username: labis
@@ -41,39 +41,39 @@ production:
 }
 
 def servers() {
-	fileOperations([fileCreateOperation(fileContent: '''# Be sure to restart your server when you modify this file.
+  fileOperations([fileCreateOperation(fileContent: '''# Be sure to restart your server when you modify this file.
 production:
   slide_server:
-	  host: 10.212.148.40
-	  port: 8081
+    host: 10.212.148.40
+    port: 8081
   mongo_server:
-	  host: 10.212.148.40
-	  port: 8088
+    host: 10.212.148.40
+    port: 8088
   req_viewer:
-	  host: 10.212.148.50
-	  port: 8082
+    host: 10.212.148.50
+    port: 8082
   redis_server:
-	  host: 10.212.148.40
-	  port: 6379
+    host: 10.212.148.40
+    port: 6379
   labis_v2:
-	  host: v2qa.labis.net
-	  port: 443
+    host: v2qa.labis.net
+    port: 443
   labis_api:
-	  host: apiqa.labis.net
-	  port: 443
+    host: apiqa.labis.net
+    port: 443
   device_server:
-	  host: localhost
-	  port: 9999
+    host: localhost
+    port: 9999
   ssrs_server:
-	  host: https://ssrsdemo.labis.net
-	  folder: "sandbox"
-	  user: administrator
-	  password: password
+    host: https://ssrsdemo.labis.net
+    folder: "sandbox"
+    user: administrator
+    password: password
 ''', fileName: 'config/servers.yml')])
 }
 
 def memcached() {
-	fileOperations([fileCreateOperation(fileContent: '''# Be sure to restart your server when you modify this file.
+  fileOperations([fileCreateOperation(fileContent: '''# Be sure to restart your server when you modify this file.
 defaults:
   readonly: false
   urlencode: false
